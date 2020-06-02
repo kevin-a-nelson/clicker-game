978258b137a761f22f3a2d721324533dc7d67c4b
Kevin Nelson <nelson67@stolaf.edu>
2020-06-01 22:49:48 -0500

    add buttons to increment and decrement count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { counterReducer } from './counter.reducer';
 
 import { AppComponent } from './app.component';
 
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
